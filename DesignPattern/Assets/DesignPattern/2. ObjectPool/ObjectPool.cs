dddf7be445f0d88872e6fbe1c120acd46082969c
sungbum05 <sblim05@naver.com>
2023-04-10 12:18:16 +0900

    Test

--- DesignPattern/Assets/DesignPattern/2. ObjectPool/ObjectPool.cs
+++ DesignPattern/Assets/DesignPattern/2. ObjectPool/ObjectPool.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class ObjectPool<T> : MonoBehaviour
+public enum PoolEnum
 {
-    QueueLSB<T> Pool = new QueueLSB<T>(100);
+    Option1,
+    Option2,
+    Option3
+}
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField]
+    public PoolEnum poolEnum;
 
     public void Initialize(int Count)
     {
@@ -16,16 +25,31 @@ public class ObjectPool<T> : MonoBehaviour
 
     public void CreateNewObj()
     {
-        Instantiate(T)
-    }
-
-    public T GetObj()
-    {
 
     }
 
-    public void ReturnObj(T Obj)
-    {
+    //public T GetObj()
+    //{
 
+    //}
+
+    //public void ReturnObj(T Obj)
+    //{
+
+    //}
+}
+
+[CustomEditor(typeof(ObjectPool))]
+public class MyScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        ObjectPool myScript = (ObjectPool)target;
+
+        // Enum 변수를 편집할 수 있는 드롭다운 목록을 생성합니다.
+        myScript.poolEnum = (PoolEnum)EditorGUILayout.EnumPopup("My Enum Variable", myScript.poolEnum);
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
