5419dcb95dffeecf50c2defd5d1822fca3b90fdd
sungbum05 <sblim05@naver.com>
2023-04-12 14:33:55 +0900

    ObjPoolInit

--- DesignPattern/Assets/DesignPattern/2. ObjectPool/ObjectPool.cs
+++ DesignPattern/Assets/DesignPattern/2. ObjectPool/ObjectPool.cs
@@ -2,29 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class ObjectPoolData
+{
+    public string Key;
+    public GameObject PoolObj;
+    public int MaxSpawnCount;
+    public int BasicSpawnCount;
+    public int CurSpawnCount;
+    public int CurActiveCount;
+
+    public Transform TrParantObj;
+    public Transform TrActiveObj;
+    public Transform TrDeactiveObj;
+}
+
 public class ObjectPool : MonoBehaviour
 {
-    Dictionary<string, QueueLSB<GameObject>> ActiveTrueMultiplePool = new Dictionary<string, QueueLSB<GameObject>>();
-    Dictionary<string, QueueLSB<GameObject>> ActiveFalseMultiplePool = new Dictionary<string, QueueLSB<GameObject>>();
+    [SerializeField]
+    private List<ObjectPoolData> PoolDataList = new List<ObjectPoolData>();
 
-    public void NewCategory(string Name, int PoolSize)
+    private Dictionary<string, ObjectPoolData> PoolSpawnData = new Dictionary<string, ObjectPoolData>();
+    private Dictionary<string, QueueLSB<GameObject>> ActiveMultiplePool = new Dictionary<string, QueueLSB<GameObject>>();
+    private Dictionary<string, QueueLSB<GameObject>> DeactiveMultiplePool = new Dictionary<string, QueueLSB<GameObject>>();
+
+    private void Start()
     {
-        QueueLSB<GameObject> Pool = new QueueLSB<GameObject>(PoolSize);
-
-
+        Initialize();
     }
 
-    public void Initialize(int Count)
+    public void Initialize()
     {
-        for (int i = 0; i < Count; i++)
+        for (int i = 0; i < PoolDataList.Count; i++)
         {
-            CreateNewObj();
+            PoolSpawnData.Add(PoolDataList[i].Key, PoolDataList[i]);
+
+            NewCategory(PoolDataList[i].Key, PoolDataList[i].MaxSpawnCount);
+            CreateNewObj(PoolDataList[i].Key, PoolDataList[i].BasicSpawnCount);
         }
     }
 
-    public void CreateNewObj()
+    public void NewCategory(string Key, int PoolSize)
     {
+        // 오브젝트 풀 딕셔너리 추가
+        QueueLSB<GameObject> ActivePool = new QueueLSB<GameObject>(PoolSize);
+        QueueLSB<GameObject> DeactivePool = new QueueLSB<GameObject>(PoolSize);
+        ActiveMultiplePool.Add(Key, ActivePool);
+        DeactiveMultiplePool.Add(Key, DeactivePool);
 
+        #region 오브젝트 스폰 장소
+        //오브젝트 스폰 장소 소환
+        GameObject Parant = new GameObject($"{Key} Parant: MaxSize[{PoolSize}], CurSpawn[{0}]");
+        Parant.transform.SetParent(transform);
+        PoolSpawnData[Key].TrParantObj = Parant.transform;
+
+        GameObject DeactiveObj = new GameObject("Deactive");
+        DeactiveObj.transform.SetParent(Parant.transform);
+        PoolSpawnData[Key].TrDeactiveObj = DeactiveObj.transform;
+
+        GameObject ActiveObj = new GameObject("Active");
+        ActiveObj.transform.SetParent(Parant.transform);
+        PoolSpawnData[Key].TrActiveObj = ActiveObj.transform;
+        #endregion
+    }
+
+    public void CreateNewObj(string Key, int Count)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            GameObject NewObj = Instantiate(PoolSpawnData[Key].PoolObj);
+
+            NewObj.SetActive(false);
+            NewObj.transform.SetParent(PoolSpawnData[Key].TrDeactiveObj);
+            DeactiveMultiplePool[Key].Push(NewObj);
+
+            PoolSpawnData[Key].CurSpawnCount++;
+        }
+
+        ParantNameSet(Key);
     }
 
     public GameObject GetObj()
@@ -35,5 +90,10 @@ public class ObjectPool : MonoBehaviour
     public void ReturnObj(GameObject Obj)
     {
 
+    }
+
+    public void ParantNameSet(string Key)
+    {
+        PoolSpawnData[Key].TrParantObj.name = $"{Key} Parant: MaxSize[{PoolSpawnData[Key].MaxSpawnCount}], CurSpawn[{PoolSpawnData[Key].CurSpawnCount}]";
     }
 }
